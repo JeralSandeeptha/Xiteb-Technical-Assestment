8a6c9fa72f98ac4b18559eac39cd1c4a1eecf4f4
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2025-03-21 13:56:47 +0530

    Bugs finxing 4

--- src/data/client-logo-section.data.ts
+++ src/data/client-logo-section.data.ts
@@ -3,22 +3,22 @@ import { ClientLogo } from "../types/model.types";
 export const clientdata: ClientLogo[] = [
         {
             id: 1,
-            image: 'https://res.cloudinary.com/djgwvmcdl/image/upload/v1742536694/apple-logo_lytkdp.png'
+            image: 'https://images-workbench.99static.com/83VoHXtLTxkkCcmKfMaE23ItuKo=/99designs-contests-attachments/56/56415/attachment_56415185'
         },
         {
             id: 2,
-            image: 'https://res.cloudinary.com/djgwvmcdl/image/upload/v1742536694/facebook_uoruce.png'
+            image: 'https://images-workbench.99static.com/HX8SNw9yW03xOYtk9Eni_v4aumA=/99designs-contests-attachments/156/156758/attachment_156758807'
         },
         {
             id: 3,
-            image: 'https://res.cloudinary.com/djgwvmcdl/image/upload/v1742536694/tiktok_m83qij.png'
+            image: 'https://images-workbench.99static.com/6RZf11NUVV-ExKEzW6fIsw5aE8E=/99designs-contests-attachments/45/45644/attachment_45644895'
         },
         {
             id: 4,
-            image: 'https://res.cloudinary.com/djgwvmcdl/image/upload/v1742536694/twitter_pjipb0.png'
+            image: 'https://images-workbench.99static.com/9NxcSkPL-CciEkOGUMTQA6bBUMM=/99designs-contests-attachments/156/156490/attachment_156490073'
         },
         {
             id: 5,
-            image: 'https://res.cloudinary.com/djgwvmcdl/image/upload/v1742536694/instagram_wdoy1w.png'
+            image: 'https://images-workbench.99static.com/Tco5aVBZRzY505UgPFMIDiI248s=/99designs-contests-attachments/43/43805/attachment_43805737'
         },
     ]
