09711ba73eaebcc16bfed0274ebd3d4ed5111a47
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2025-03-21 14:57:29 +0530

    Added Hero section

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,9 +1,20 @@
+import './Hero.scss';
+
 const Hero = () => {
   return (
-    <div>
-      
+    <div className='hero'>
+      <div className="hero-left">
+        <div className="content">
+          <h5 className='sub-title'>LOGISTIC</h5>
+          <h1 className='title'>Best Shipping <span>Partner</span></h1>
+          <h5 className='hero-description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </h5>
+          <button className='cta'>DISCOVER MORE</button>
+        </div>
+      </div>
+      <div className="hero-right"></div>
     </div>
   )
 }
 
-export default Hero
+export default Hero;
