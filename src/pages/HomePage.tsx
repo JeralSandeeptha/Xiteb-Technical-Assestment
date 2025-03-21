b14386b71788bd5ed209c005d312fbb5356269e3
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2025-03-21 13:43:57 +0530

    Bugs finxing 2

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,24 @@
 import ClientLogoSection from "../components/client-logo-section/ClientLogoSection";
+import Footer from "../components/footer/Footer";
+import Hero from "../components/hero/Hero";
 import NewsSection from "../components/news-section/NewsSection";
+import Services from "../components/services/Services";
 import StatsContent from "../components/stats-content/StatsContent";
+import Testimonials from "../components/testimonials/Testimonials";
 import WelcomeSection from "../components/welcome-section/WelcomeSection";
 
 const HomePage = () => {
 
   return (
     <>
+      <Hero />
       <WelcomeSection />
       <StatsContent />
+      <Services />
+      <Testimonials />
       <ClientLogoSection />
       <NewsSection />
+      <Footer />
     </>
   );
 
